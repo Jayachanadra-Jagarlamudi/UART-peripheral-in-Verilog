1feb2661a08befa924cfebdbc26ca6c6402d614b
Jayachanadra-Jagarlamudi <136566307+Jayachanadra-Jagarlamudi@users.noreply.github.com>
2025-11-29 22:28:38 +0530

    Update uart_memory_interfaces.v

--- uart_memory_interfaces.v
+++ uart_memory_interfaces.v
@@ -5,110 +5,68 @@
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-module TX_memory_interface(
-    input				clk, tx_ena, write_req, rx_rts,
-    input reg	[1:0]	tx_state,
-    input reg	[9:0]	write_word,
-    output reg	[9:0]	tx_word,
-    output reg			tx_loaded,
-    output				mem_FULL
+module FIFO_autoreader_with_ecc(
+	input clk, rst, load_begin, fifo_read,
+	input [15:0] start_addr,
+	input [3:0] addr_inc,
+	input [6:0] data_struct, // [0:1] = #bytes per line, [6:3] = #lines to copy
+	input [63:0] data_in,
+	output mem_read_req,
+	output reg [15:0] read_addr,
+	output reg [15:0] data_out,
+	output fifo_full, fifo_empty
 );
-    reg [15:0][9:0] TX_mem ;
-    reg [3:0] TX_mem_addr ;
-    wire mem_EMPTY ;
-    reg tx_load_count ;
-    
-    `probe(TX_mem[3:0]);
-    `probe(TX_mem_addr);
-    `probe(mem_EMPTY);
-        
-    assign mem_EMPTY = ~|TX_mem_addr ;
-    assign mem_FULL = &TX_mem_addr ;
-    
-    always@(posedge clk) begin
-        if(~tx_ena) begin
-            TX_mem <= 160'b0 ;
-            tx_loaded <= 1'b0 ;
-            TX_mem_addr <= 4'd0 ;
-            tx_word <= 10'b0 ;
-            tx_load_count <= 1'b0 ;
-        end
-        else begin
-            if(write_req) begin
-                TX_mem[TX_mem_addr] <= write_word;
-                TX_mem_addr <= TX_mem_addr + 4'b1 ;
-                tx_loaded <= tx_load_count ? tx_loaded : 1'b0 ;
-                tx_load_count <= 1'b0 ;
-            end
-            else begin
-                if(~mem_EMPTY & ~|tx_state & ~tx_loaded & rx_rts) begin
-                    tx_word <= TX_mem[TX_mem_addr-1'b1] ;
-                    TX_mem_addr <= TX_mem_addr - 1'b1 ;
-                    tx_loaded <= 1'b1 ;
-                    tx_load_count <= 1'b1 ;
-                end
-                else begin
-                    tx_loaded <= tx_load_count ? tx_loaded : 1'b0 ;
-                    tx_load_count <= 1'b0 ;
-                end
-            end
-        end
-    end
-    
-endmodule
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
- UART RX buffer interface
-  - retrieves data from RX module if present and stores in FIFO
-*/
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-module RX_memory_interface(
-    input				clk, rx_ena, read_req,
-    input reg	[2:0]	rx_status,
-    input reg	[9:0]	rx_word,
-    output reg	[9:0]	read_word,
-    output reg			data_ack,
-    output				mem_FULL
-);
-    reg [15:0][15:0] RX_mem ;
-    reg [3:0] RX_mem_addr ;
-    wire mem_EMPTY ;
-    reg rx_data_ready, rx_data_ready_pre ;
-    
-    `probe(RX_mem[3:0]);
-    `probe(RX_mem_addr);
-    `probe(mem_EMPTY);
-        
-    assign mem_EMPTY = ~|RX_mem_addr ;
-    assign mem_FULL = &RX_mem_addr ;
-    
-    always@(posedge clk) begin
-        rx_data_ready_pre <= (rx_status == 2'd2) ;
-        rx_data_ready <= (rx_data_ready_pre) ;
-        if(~rx_ena) begin
-            RX_mem <= 160'b0 ;
-            RX_mem_addr <= 4'd0 ;
-            data_ack <= 1'b0 ;
-        end
-        else begin
-            if(read_req) begin
-                read_word <= RX_mem[RX_mem_addr - 4'b1] ;
-                RX_mem_addr <= RX_mem_addr - 4'b1 ;
-                data_ack <= 1'b0 ;
-            end
-            else begin
-                if(~mem_FULL & rx_data_ready) begin
-                    RX_mem[RX_mem_addr] <= {6'b0, rx_word} ;
-                    RX_mem_addr <= RX_mem_addr + 1'b1 ;
-                    data_ack <= 1'b1 ;
-                end
-                else begin
-                    data_ack <= 1'b0 ;
-                end
-            end
-        end
-    end
-    
+	
+	reg [11:0] FIFO [0:31] ;
+	reg [5:0] wptr, rptr, ptr_jump;
+	reg [2:0] byte_counter ;
+	reg [3:0] word_counter; 
+	reg [1:0] fifo_state;
+	reg [63:0] read_data ;
+	
+	always@(posedge clk) begin
+		read_data <= data_in ;
+		ptr_jump <= (5'b1 << data_struct[1:0]) - 1'b1 ;
+		if(rst) begin
+			wptr <= 6'b0 ;
+			rptr <= 6'b0 ;
+			word_counter <= 4'b0 ;
+			read_addr <= 16'b0 ;
+			data_out <= 12'b0 ;
+			fifo_state <= 2'b0 ;
+		end
+		else begin
+			case(fifo_state) // FIFO WRITING FROM MEM FSM
+				2'b00 : begin // IDLE
+					fifo_state <= load_begin ? 2'b01 : 2'b00 ;
+					word_counter <= 4'b0 ;
+					read_addr <= start_addr ;
+				end
+				2'b01 : begin // DATA READ REQ
+					byte_counter <= 3'b0 ;
+					read_addr <= read_addr + addr_inc ;
+					word_counter <= word_counter + {3'b0,~fifo_full} ;
+					fifo_state <= fifo_full ? 2'b10 : 2'b11 ;
+				end
+				2'b10 : begin // WAIT FOR FIFO TO NOT BE FULL
+					fifo_state <= fifo_full ? 2'b10 : 2'b11 ;
+				end
+				2'b11 : begin // READ DATA FROM MEM
+					wptr <= wptr + 1'b1 ;
+					byte_counter <= byte_counter + 1'b1 ;
+					fifo_state <= (byte_counter==ptr_jump[3:0]) ? ( (word_counter==data_struct[6:2]) ? 2'b00 : 2'b01) : 2'b11 ;
+					FIFO[wptr] <= {^(read_data[7:0] & 8'hDA), ^(read_data[7:0] & 8'hB6), read_data[7], ^(read_data[7:0] & 8'h71), read_data[6:4], ^(read_data[7:0] & 8'h0f), read_data[3:0]} ;
+				end
+			endcase
+			
+			// FIFO WRITING TO TX MODULE FSM
+			rptr <= rptr + ~fifo_empty & fifo_read ;
+			data_out <= {3'b0, FIFO[rptr]} ;
+		end
+	end
+		
+	assign fifo_full = ( {~rptr[5],rptr[4:0]} <= (wptr+ptr_jump) ) ;
+    assign fifo_empty = ( wptr == rptr ) ;
+	assign mem_read_req = (fifo_state == 2'b01) & ~fifo_full ;
+	
 endmodule
